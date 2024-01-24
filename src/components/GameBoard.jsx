172515b02ac7dcaf43f561752ad8f60cfd9b51c5
braedengoodwin <braedenagoodwin@hotmail.com>
2024-01-23 20:49:23 -0600

    added a lot

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
+
 const initalGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
-export default function GameBoard(){
-    return(
-        <ol id="game-board">
-            {initalGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
-                </ol>
-            </li>)}
-        </ol>
-    );
+export default function GameBoard({ onSelectSquare, turns  }) {
+  let gameBoard = initalGameBoard;
+
+  for (const turn of turns){
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+//   const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
+//   function handleSelectSquare(rowIndex, colIndex) {
+//     setGameBoard((prevGameBoard) => {
+//       const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray]),];
+//       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+//       return updatedBoard;
+//     });
+
+//     onSelectSquare();
+//   }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
---
+++ src/components/Log.jsx
@@ -0,0 +1,9 @@
+export default function Log({ playerMove }) {
+  return (
+    <ol id="log">
+      {playerMove.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row}, {turn.square.col}</li>
+      ))}
+    </ol>
+  );
+}
